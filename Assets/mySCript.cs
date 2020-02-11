58588bdc6936ce1a430cfa02ac1f301e41ad127c
thomastyl <60270930+thomastyl@users.noreply.github.com>
2020-02-11 13:59:15 -0600

    nextCommit
    test descript

---
+++ Assets/mySCript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mySCript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Example code goes here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
